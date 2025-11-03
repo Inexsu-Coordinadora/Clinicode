807f414411c4ab4226d88c3cc3ea73595e6730f2
DiegoHiguita <diegohiguita1993@hotmail.com>
2025-11-03 09:37:52 -0500

    Implementacion de CRUD completo

--- src/core/aplicacion/casoUsoMedico/IMedicoCasosUso.ts
+++ src/core/aplicacion/casoUsoMedico/IMedicoCasosUso.ts
@@ -1,9 +1,10 @@
 import { IMedico } from "../../dominio/entidades/medicos/IMedico.js";
+import { MedicoDTO } from "../../infraestructura/esquemas/MedicoEsquema.js";
 
-export interface IMedicosRepositorio {
-  crearMedico(datosMedico: IMedico): Promise<string>;
+export interface IMedicoCasosUso {
   listarMedicos(limite?: number): Promise<IMedico[]>;
   obtenerMedicoPorId(idMedico: string): Promise<IMedico | null>;
-  actualizarMedico(idMedico: string, datosMedico: IMedico): Promise<IMedico>;
+  crearMedico(medico: MedicoDTO): Promise<string>;
+  actualizarMedico(idMedico: string, medico: IMedico): Promise<IMedico | null>;
   eliminarMedico(idMedico: string): Promise<void>;
 }
